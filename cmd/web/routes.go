d07a9d5c6ae25abbce668972091cd35ed6c0110a
BonillaDABI <79613748+Bonilla7784@users.noreply.github.com>
2023-11-17 15:44:12 -0600

    Final commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,46 +11,46 @@ import (
 )
 
 func routes(app *config.AppConfig, s *handlers.Hub) http.Handler {
-    mux := chi.NewRouter()
+	mux := chi.NewRouter()
 
-    // middleware
-    mux.Use(middleware.Recoverer)
-    mux.Use(SessionLoad)
+	// middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(SessionLoad)
 
-    // Serve static files
-    fileServer := http.FileServer(http.Dir("templates/static"))
-    mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+	// Serve static files
+	fileServer := http.FileServer(http.Dir("templates/static"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
-    // routes
-    mux.Get("/register", http.HandlerFunc(handlers.Repo.Register))
-    mux.Post("/register", http.HandlerFunc(handlers.Repo.PostRegister))
+	// routes
+	mux.Get("/register", http.HandlerFunc(handlers.Repo.Register))
+	mux.Post("/register", http.HandlerFunc(handlers.Repo.PostRegister))
 
-    mux.Get("/login", http.HandlerFunc(handlers.Repo.Login))
-    mux.Post("/login", http.HandlerFunc(handlers.Repo.PostLogin))
+	mux.Get("/login", http.HandlerFunc(handlers.Repo.Login))
+	mux.Post("/login", http.HandlerFunc(handlers.Repo.PostLogin))
 
-    mux.With(IsLoggedIn).Get("/logout", http.HandlerFunc(handlers.Repo.Logout))
+	mux.With(IsLoggedIn).Get("/logout", http.HandlerFunc(handlers.Repo.Logout))
 
-    mux.With(IsLoggedIn).Get("/", http.HandlerFunc(handlers.Repo.Dashboard))
+	mux.With(IsLoggedIn).Get("/", http.HandlerFunc(handlers.Repo.Dashboard))
 
-    mux.With(IsLoggedIn).Get("/create-trip", http.HandlerFunc(handlers.Repo.CreateTrip))
-    mux.With(IsLoggedIn).Post("/create-trip", http.HandlerFunc(handlers.Repo.PostCreateTrip))
+	mux.With(IsLoggedIn).Get("/create-trip", http.HandlerFunc(handlers.Repo.CreateTrip))
+	mux.With(IsLoggedIn).Post("/create-trip", http.HandlerFunc(handlers.Repo.PostCreateTrip))
 
-    mux.With(IsLoggedIn).Get("/trips", http.HandlerFunc(handlers.Repo.ActiveTrips))
+	mux.With(IsLoggedIn).Get("/trips", http.HandlerFunc(handlers.Repo.ActiveTrips))
 
-    // Add the new route for searching trips by departure_time
-    mux.With(IsLoggedIn).Post("/searchTrips", http.HandlerFunc(handlers.Repo.SearchTripsHandler))
+	// Add the new route for searching trips by departure_time
+	mux.With(IsLoggedIn).Post("/searchTrips", http.HandlerFunc(handlers.Repo.SearchTripsHandler))
 
-    mux.With(IsLoggedIn).Get("/account", http.HandlerFunc(handlers.Repo.ShowSettings))
-    mux.With(IsLoggedIn).Post("/account/change-password", http.HandlerFunc(handlers.Repo.ChangePassword))
+	mux.With(IsLoggedIn).Get("/account", http.HandlerFunc(handlers.Repo.ShowSettings))
+	mux.With(IsLoggedIn).Post("/account/change-password", http.HandlerFunc(handlers.Repo.ChangePassword))
 
-    mux.With(IsLoggedIn).Get("/join-trip/{tripId}", http.HandlerFunc(handlers.Repo.JoinTrip))
-    mux.With(IsLoggedIn).Get("/leave-trip/{tripId}", http.HandlerFunc(handlers.Repo.LeaveTrip))
+	mux.With(IsLoggedIn).Get("/join-trip/{tripId}", http.HandlerFunc(handlers.Repo.JoinTrip))
+	mux.With(IsLoggedIn).Get("/leave-trip/{tripId}", http.HandlerFunc(handlers.Repo.LeaveTrip))
 
-    mux.With(IsLoggedIn).Handle("/ws/chat/{tripId}/{userId}", websocket.Handler(s.HandleWs))
-    mux.With(IsLoggedIn).Get("/chat/{tripId}/{userId}", handlers.Repo.RenderChat)
-    mux.With(IsLoggedIn).Get("/chat/json/{tripId}/{userId}", handlers.Repo.RenderChatPage)
+	mux.With(IsLoggedIn).Handle("/ws/chat/{tripId}/{userId}", websocket.Handler(s.HandleWs))
+	mux.With(IsLoggedIn).Get("/chat/{tripId}/{userId}", handlers.Repo.RenderChat)
+	mux.With(IsLoggedIn).Get("/chat/json/{tripId}/{userId}", handlers.Repo.RenderChatPage)
 
-    mux.With(IsLoggedIn).Post("/update-group-name", http.HandlerFunc(handlers.Repo.UpdateChatName))
+	mux.With(IsLoggedIn).Post("/update-group-name", http.HandlerFunc(handlers.Repo.UpdateChatName))
 
-    return mux
+	return mux
 }
--- pkg/handlers/trip_handlers.go
+++ pkg/handlers/trip_handlers.go
@@ -1,87 +1,92 @@
 package handlers
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "strconv"
-    "time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 
-    "github.com/go-chi/chi"
-    "github.com/mariogarzac/tecpool/pkg/db"
-    "github.com/mariogarzac/tecpool/pkg/models"
-    "github.com/mariogarzac/tecpool/pkg/render"
+	"github.com/go-chi/chi"
+	"github.com/mariogarzac/tecpool/pkg/db"
+	"github.com/mariogarzac/tecpool/pkg/models"
+	"github.com/mariogarzac/tecpool/pkg/render"
 )
 
 func (m *Repository) CreateTrip(w http.ResponseWriter, r *http.Request) {
-    render.RenderTemplate(w, r, "dashboard.page.html", &models.TemplateData{})
+	render.RenderTemplate(w, r, "dashboard.page.html", &models.TemplateData{})
 }
 
 func (m *Repository) PostCreateTrip(w http.ResponseWriter, r *http.Request) {
-    carModel := r.FormValue("car_model")
-    licensePlate := r.FormValue("plate")
-    departureTime := r.FormValue("departure_time")
-    userId := m.App.Session.GetInt(r.Context(), "userId")
+	carModel := r.FormValue("car_model")
+	licensePlate := r.FormValue("plate")
+	departureTime := r.FormValue("departure_time")
+	userId := m.App.Session.GetInt(r.Context(), "userId")
+	street := r.FormValue("street")
+	number := r.FormValue("number")
+	postalCode := r.FormValue("postalCode")
 
-    err := db.CreateTrip(carModel, licensePlate, departureTime, userId)
-    if err != nil {
-        log.Fatal("Error creating trip", err)
-    }
+	startLocation := street + " " + number + ", " + postalCode
 
-    // Add the user who created the trip to the trip
-    tripId, err := db.GetTripID()
-    if err != nil {
-        log.Println("Error getting tripId: ", err)
-        return 
-    }
+	err := db.CreateTrip(carModel, licensePlate, departureTime, startLocation, userId)
+	if err != nil {
+		log.Fatal("Error creating trip", err)
+	}
 
-    err  = db.AddUserToTrip(uint64(userId), uint64(tripId))
-    if err != nil {
-        log.Println("Error adding user to trip:", err)
-        return 
-    }
+	// Add the user who created the trip to the trip
+	tripId, err := db.GetTripID()
+	if err != nil {
+		log.Println("Error getting tripId: ", err)
+		return
+	}
 
-    // create a chat room with that trip id
-    // m.CreateTripRoom(w, r)
+	err = db.AddUserToTrip(uint64(userId), uint64(tripId))
+	if err != nil {
+		log.Println("Error adding user to trip:", err)
+		return
+	}
 
-    http.Redirect(w, r, "/", http.StatusSeeOther)
+	// create a chat room with that trip id
+	// m.CreateTripRoom(w, r)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // Adds user to the trip in the db
-func (m *Repository) JoinTrip(w http.ResponseWriter, r *http.Request){
+func (m *Repository) JoinTrip(w http.ResponseWriter, r *http.Request) {
 
-    // Get id from the url and cast it into an int
-    id := chi.URLParam(r, "tripId")
-    tripId,err := strconv.Atoi(id)
+	// Get id from the url and cast it into an int
+	id := chi.URLParam(r, "tripId")
+	tripId, err := strconv.Atoi(id)
 
-    if err != nil {
-        log.Println("Error getting tripId ", err)
-        return
-    }
+	if err != nil {
+		log.Println("Error getting tripId ", err)
+		return
+	}
 
-    // Get user info from cookies
-    userId := m.App.Session.GetInt(r.Context(), "userId")
+	// Get user info from cookies
+	userId := m.App.Session.GetInt(r.Context(), "userId")
 
-    if err != nil {
-        log.Println("Error getting userId: ", err)
-        return
-    }
+	if err != nil {
+		log.Println("Error getting userId: ", err)
+		return
+	}
 
-    // Check if the trip exists
-    if tripExists := db.DoesTripExist(tripId); !tripExists {
-        log.Println("Room does not exist")
-        http.Redirect(w, r, "/", http.StatusSeeOther)
-        return
-        // show error message
-    }
+	// Check if the trip exists
+	if tripExists := db.DoesTripExist(tripId); !tripExists {
+		log.Println("Room does not exist")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+		// show error message
+	}
 
-    // Check if user is already in the trip
-    if inTrip := db.IsUserInTrip(tripId, userId); inTrip {
-        log.Println("User is already in the room")
-        http.Redirect(w, r, "/", http.StatusSeeOther)
-        return
-        // show error message
-    }
+	// Check if user is already in the trip
+	if inTrip := db.IsUserInTrip(tripId, userId); inTrip {
+		log.Println("User is already in the room")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+		// show error message
+	}
 
     if usersInTrip := db.CountUsersInTrip(tripId); usersInTrip > 4 {
         log.Println("The trip already has 4 users")
@@ -102,36 +107,82 @@ func (m *Repository) JoinTrip(w http.ResponseWriter, r *http.Request){
         return
     }
 
-    // Add user to trip
-    err = db.AddUserToTrip(uint64(userId), uint64(tripId));
+	// Add user to trip
+	err = db.AddUserToTrip(uint64(userId), uint64(tripId))
 
-    if err != nil {
-        log.Println("Error adding user to trip: ", err)
-        return
-        // show error message
-    }
+	if err != nil {
+		log.Println("Error adding user to trip: ", err)
+		return
+		// show error message
+	}
 
-    http.Redirect(w, r, "/", http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func (m *Repository) ActiveTrips(w http.ResponseWriter, r *http.Request){
+func (m *Repository) ActiveTrips(w http.ResponseWriter, r *http.Request) {
 
-    userId := m.App.Session.GetInt(r.Context(), "userId")
+	userId := m.App.Session.GetInt(r.Context(), "userId")
 
-    userTrips := make(map[int]*models.Trip)
+	userTrips := make(map[int]*models.Trip)
 
+	// Get user trips from the db
+	userTrips, err := db.GetUserTrips(userId)
+	if err != nil {
+		log.Println("Error getting user trips: ", err)
+		return
+	}
 
-    // Get user trips from the db
-    userTrips, err := db.GetUserTrips(userId)
-    if err != nil {
-        log.Println("Error getting user trips: ", err)
-        return 
-    }
+	// Render trips in the active trips page
+	render.RenderTemplate(w, r, "trips.page.html", &models.TemplateData{
+		UserTrips: userTrips,
+	})
+}
 
-    // Render trips in the active trips page
-    render.RenderTemplate(w, r, "trips.page.html", &models.TemplateData{
-        UserTrips: userTrips,
-    })
+func (m *Repository) SearchTripsHandler(w http.ResponseWriter, r *http.Request) {
+	var requestData map[string]string
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		log.Println("Error decoding request body:", err)
+		http.Error(w, `{"error": "Invalid request format"}`, http.StatusBadRequest)
+		return
+	}
+
+	departureTimeString, ok := requestData["departureTime"]
+	if !ok || departureTimeString == "" {
+		log.Println("Error: Departure time is empty.")
+		http.Error(w, `{"error": "Please provide a valid departure time."}`, http.StatusBadRequest)
+		return
+	}
+
+	departureTime, err := time.Parse("2006-01-02T15:04", departureTimeString)
+	if err != nil {
+		log.Println("Error parsing departure time:", err)
+		http.Error(w, `{"error": "Invalid departure time format"}`, http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Departure time", departureTime)
+
+	// Fetch trips from the database that match the given departure time
+	trips, err := db.SearchTripsByDepartureTime(departureTime)
+	if err != nil {
+		log.Println("Error fetching trips by departure time:", err)
+		http.Error(w, `{"error": "Internal server error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	for i := range trips {
+		log.Println("Fetched trips ", trips[i].CarModel)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(trips); err != nil {
+		log.Println("Error encoding trips to JSON:", err)
+		http.Error(w, `{"error": "Internal server error"}`, http.StatusInternalServerError)
+	}
+
+    
+
 }
 
 func (m *Repository) LeaveTrip(w http.ResponseWriter, r *http.Request) {
@@ -153,49 +204,3 @@ func (m *Repository) LeaveTrip(w http.ResponseWriter, r *http.Request) {
 
     http.Redirect(w, r, "/trips", http.StatusSeeOther)
 }
-
-func (m *Repository) SearchTripsHandler(w http.ResponseWriter, r *http.Request) {
-    var requestData map[string]string
-    err := json.NewDecoder(r.Body).Decode(&requestData)
-    if err != nil {
-        log.Println("Error decoding request body:", err)
-        http.Error(w, `{"error": "Invalid request format"}`, http.StatusBadRequest)
-        return
-    }
-
-    departureTimeString, ok := requestData["departureTime"]
-    if !ok || departureTimeString == "" {
-        log.Println("Error: Departure time is empty.")
-        http.Error(w, `{"error": "Please provide a valid departure time."}`, http.StatusBadRequest)
-        return
-    }
-
-    departureTime, err := time.Parse("2006-01-02T15:04", departureTimeString)
-    if err != nil {
-        log.Println("Error parsing departure time:", err)
-        http.Error(w, `{"error": "Invalid departure time format"}`, http.StatusBadRequest)
-        return
-    }
-
-    log.Println("Departure time", departureTime)
-
-    // Fetch trips from the database that match the given departure time
-    trips, err := db.SearchTripsByDepartureTime(departureTime)
-    if err != nil {
-        log.Println("Error fetching trips by departure time:", err)
-        http.Error(w, `{"error": "Internal server error"}`, http.StatusInternalServerError)
-        return
-    }
-
-    for i := range trips {
-        log.Println("Fetched trips ", trips[i].CarModel)
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    if err := json.NewEncoder(w).Encode(trips); err != nil {
-        log.Println("Error encoding trips to JSON:", err)
-        http.Error(w, `{"error": "Internal server error"}`, http.StatusInternalServerError)
-    }
-
-}
-
